b97674a3ca985be9be691915feba3b94cefd3bc5
初志鑫 <1204505056@qq.com>
2020-07-04 22:55:52 +0800

    deploy

--- src/layouts/components/Ad/index.vue
+++ src/layouts/components/Ad/index.vue
@@ -1,19 +1,19 @@
 <template>
-  <el-carousel
-    v-if="adList"
-    height="30px"
-    direction="vertical"
-    :autoplay="true"
-    :interval="3000"
-    indicator-position="none"
-  >
-    <el-carousel-item v-for="(item, index) in adList" :key="index">
-      <div class="vab-ad">
+  <div class="vab-ad">
+    <el-carousel
+      v-if="adList"
+      height="30px"
+      direction="vertical"
+      :autoplay="true"
+      :interval="3000"
+      indicator-position="none"
+    >
+      <el-carousel-item v-for="(item, index) in adList" :key="index">
         <el-tag type="warning"> Ad</el-tag>
         <a target="_blank" :href="item.url"> {{ item.title }}</a>
-      </div>
-    </el-carousel-item>
-  </el-carousel>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 <script>
 import { getList } from "@/api/ad";
@@ -42,6 +42,7 @@ export default {
   line-height: 30px;
   cursor: pointer;
   background: #eef1f6;
+  box-shadow: 0 -1px 2px rgba(0, 21, 41, 0.08) inset;
 
   a {
     color: #999;
