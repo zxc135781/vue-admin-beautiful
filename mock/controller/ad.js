ac3f05b007d73a3a2ea38ae9de9850904623e959
初志鑫 <1204505056@qq.com>
2020-07-10 13:33:46 +0800

    deploy

--- mock/controller/ad.js
+++ mock/controller/ad.js
@@ -1,6 +1,11 @@
 const data = [
   {
     title:
+      "作为一个程序员，我迄今为止最骄傲的事情：2020年7月10日，vue/cli4作者蒋豪群采纳了我的对sass-loader 9.0全局注入变量的文档修改建议，以后全世界都能看到我的名字了，这远比vue-admin-beautiful更让我有成就感，感谢shaonialife的帮助。",
+    url: "https://github.com/vuejs/vue-cli/blob/master/docs/zh/guide/css.md",
+  },
+  {
+    title:
       "近日发现一个名为OKMG芒果源码的网站公然出售vue-admin-beautiful的开源代码，在此向大家说明，框架开源版本永久免费，请勿上当受骗。",
     url:
       "//shang.qq.com/wpa/qunwpa?idkey=14f123ac6d4ef3122bbb60d4693f1d8c951a50be2296951efb12d5ab1642f36b",
